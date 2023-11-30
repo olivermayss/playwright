1b4c2de9d5786cf05a7eb286e5486a4ed680fbb2
Mayra <mayra.moura@atile.com.br>
2023-11-29 21:01:00 -0300

    Subida de projeto

---
+++ tests/example.spec.js
@@ -0,0 +1,52 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/')
+})
+
+test('Login com sucesso @login', async ({ page }) => {
+
+  const texto = await page.waitForSelector('text = NEWSLETTER')
+  await texto.scrollIntoViewIfNeeded()
+ 
+  await page.getByRole('link', { name: ' Login' }).click()
+  await page.locator('#user').click()
+  await page.locator('#user').fill('teste@gmail.com')
+  await page.screenshot({ path: 'screenshot/screenshot1.png' })
+  await page.locator('#password').click()
+  await page.locator('#password').fill('123456')
+  await page.locator('#password').screenshot({ path: 'screenshot/elementosenha.png' })
+  await page.screenshot({ path: 'screenshot/screenshot2.png' })
+  await page.getByRole('button', { name: 'login' }).click()
+});
+
+test('Login com sucesso 2 @login', async ({ page }) => {
+ 
+  await page.getByRole('link', { name: ' Login' }).click()
+  await page.locator('#user').click()
+  await page.locator('#user').fill('teste@gmail.com')
+  await page.screenshot({ path: 'screenshot/screenshot1.png' })
+  await page.locator('#password').click()
+  await page.locator('#password').fill('123456')
+  await page.locator('#password').screenshot({ path: 'screenshot/elementosenha.png' })
+  await page.screenshot({ path: 'screenshot/screenshot2.png' })
+  await page.getByRole('button', { name: 'login' }).click()
+});
+
+test('Login com sucesso 3 @login', async ({ page }) => {
+ 
+  await page.getByRole('link', { name: ' Login' }).click()
+  await page.locator('#user').click()
+  await page.locator('#user').fill('teste@gmail.com')
+  await page.screenshot({ path: 'screenshot/screenshot1.png' })
+  await page.locator('#password').click()
+  await page.locator('#password').fill('123456')
+  await page.locator('#password').screenshot({ path: 'screenshot/elementosenha.png' })
+  await page.screenshot({ path: 'screenshot/screenshot2.png' })
+  await page.getByRole('button', { name: 'login' }).click()
+});
+
+test.afterEach(async ({ page }) => {
+  // codigo nesta parte
+})
